5b5842c4155309bf97d2155bc32a2d43710c7742
VigneshRav <vignesh212000@gmail.com>
2025-05-25 17:16:21 +0530

    Login User completed & API tested via Postman

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -11,11 +11,38 @@ export const registerUser = async (req, res) => {
     const { UserName, Email, Password } = req.body;
     const hashPassword = await bcrypt.hash(Password, 10);
     const newUser = new Users({ UserName, Email, Password: hashPassword });
-    newUser.save();
+    await newUser.save();
     res
       .status(200)
       .json({ message: "User Registered Successfully", data: newUser });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+//User Login or Sign In Function:
+export const loginUser = async (req, res) => {
+  try {
+    const { Email, Password } = req.body;
+    const existingUser = await Users.findOne({ Email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+    const passwordMatch = await bcrypt.compare(Password, existingUser.Password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid Password" });
+    }
+    const token = jwt.sign(
+      { _id: existingUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    existingUser.Token = token;
+    await existingUser.save();
+    res
+      .status(200)
+      .json({ message: "User LoggedIn Successfully", token: token });
+  } catch (error) {
+    res.status(200).json({ message: error.message });
   }
 };
--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../Controllers/userController.js";
+import { loginUser, registerUser } from "../Controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
